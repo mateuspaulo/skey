c3addf8aaeb90b26d6af557de5a812e1689f4767
ambplan-mateus <mateus@ambplan.com.br>
2017-10-27 16:09:42 -0200

    Start :)

---
+++ skey.js
@@ -0,0 +1,48 @@
+
+document.addEventListener ("keydown", function (zEvent) {
+
+    //CTRL + ALT + 1
+    if (zEvent.ctrlKey && zEvent.altKey && zEvent.keyCode == "49") {
+        alert("CTRL + ALT + 1");
+    }
+
+    //CTRL + ALT + 2
+    if (zEvent.ctrlKey && zEvent.altKey && zEvent.keyCode == "50") {
+        alert("CTRL + ALT + 2");    
+    }
+
+    //CTRL + ALT + 3
+    if (zEvent.ctrlKey && zEvent.altKey && zEvent.keyCode == "51") {
+        alert("CTRL + ALT + 3");
+    }
+
+    //CTRL + ALT + 4
+    if (zEvent.ctrlKey && zEvent.altKey && zEvent.keyCode == "52") {
+        alert("CTRL + ALT + 4");
+    }
+
+    //CTRL + ALT + 5
+    if (zEvent.ctrlKey && zEvent.altKey && zEvent.keyCode == "53") {
+        alert("CTRL + ALT + 5");
+    }
+
+    //CTRL + ALT + 6
+    if (zEvent.ctrlKey && zEvent.altKey && zEvent.keyCode == "54") {
+        alert("CTRL + ALT + 6");
+    }
+
+    //CTRL + ALT + 7
+    if (zEvent.ctrlKey && zEvent.altKey && zEvent.keyCode == "55") {
+        alert("CTRL + ALT + 7");
+    }
+
+    //CTRL + ALT + 8
+    if (zEvent.ctrlKey && zEvent.altKey && zEvent.keyCode == "56") {
+        alert("CTRL + ALT + 8");
+    }
+
+    //CTRL + ALT + 9
+    if (zEvent.ctrlKey && zEvent.altKey && zEvent.keyCode == "57") {
+        alert("CTRL + ALT + 9");
+    }
+});
